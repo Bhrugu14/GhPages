5fadfc34df0d2324602b665de41cceabf27c6d77
Bhrugu14 <knowbhrugu14@gmail.com>
2022-09-18 01:12:45 +0530

    final commit

--- pages/index.js
+++ pages/index.js
@@ -1,69 +1,7 @@
-import { ToastContainer, toast } from "react-toastify";
-import { useRouter } from "next/router";
-import { RippleButton } from "../component";
-
 export default function Home() {
-  const router = useRouter();
   return (
-    <div className="wrapper">
-      <p>Ripple Buttons with Toast</p>
-      <section
-        style={{
-          display: "flex",
-          flex: 1,
-          flexDirection: "row",
-          height: "40%",
-          width: "100%",
-          flexWrap: "wrap",
-        }}
-      >
-        <RippleButton
-          style={{ backgroundColor: "grey" }}
-          onClickButton={() => toast.info("Information Toast!")}
-          title={"info"}
-        />
-        <RippleButton
-          style={{ backgroundColor: "rgb(21, 160, 3" }}
-          onClickButton={() => toast.success("Success Toast!")}
-          title={"success"}
-        />
-        <RippleButton
-          style={{ backgroundColor: "red" }}
-          onClickButton={() => toast.error("Warning Toast!")}
-          title={"error"}
-        />
-        <RippleButton
-          style={{ backgroundColor: "rgb(131, 129, 2)" }}
-          onClickButton={() => toast.warning("Warning Toast!")}
-          title={"warning"}
-        />
-      </section>
-      <section
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          flexDirection: "row",
-          height: "40%",
-          width: "100%",
-        }}
-      >
-        <RippleButton
-          style={{ alignSelf: "center" }}
-          onClickButton={() => router.push("/multiCarousel")}
-          title={"Next Page"}
-        />
-      </section>
-
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        draggable
-      />
+    <div>
+      <h2>github Pages deploy Demo</h2>
     </div>
   );
 }
