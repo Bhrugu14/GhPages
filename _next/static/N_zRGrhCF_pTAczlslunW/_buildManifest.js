5ad08c18ae6ab12393e33ae70993ab3364c49d47
Bhrugu14 <79759496+Bhrugu14@users.noreply.github.com>
2022-09-17 19:46:05 +0000

    Deploying to gh-pages from @ Bhrugu14/GhPages@13a766a3fac6abf9433a0a134a5808f44850ad46 🚀

--- _next/static/N_zRGrhCF_pTAczlslunW/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-57b75d8d7d36cd22.js"],"/_error":["static/chunks/pages/_error-fb68742d3cf986b6.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
