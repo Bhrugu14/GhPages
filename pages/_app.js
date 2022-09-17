7a4fe68cdbe4b7be6f984a28a6721ca08653eb20
Bhrugu14 <knowbhrugu14@gmail.com>
2022-09-17 23:22:47 +0530

    initial

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import "../styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
+1;
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
